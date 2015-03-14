74b6f934fa49a66156510314431508472877b9ce
wackyapples <wackyapples@users.noreply.github.com>
2015-03-13 17:48:39 -0700

    Cleaned up old discarded code

--- js/viz.js
+++ js/viz.js
@@ -18,51 +18,6 @@ var chartText = {
 // This feels.... dirty
 function drawVisualization() {
 
-    // Query handler, processes the data from a response
-    // function handleQueryResponse(response) {
-
-    //     // Respond to errors, displays an alert
-    //     if (response.isError()) {
-    //         alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-    //         return;
-    //     }
-
-    //     var data = response.getDataTable();
-    //     // console.log(data)
-    //     data.setColumnLabel(0, chartText.colLabel);
-    //     var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
-    //     // var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
-    //     chart.draw(data, options);
-    // };
-
-    // var opts = {
-    //     sendMethod: 'auto'
-    // };
-
-    // Create new Fusion Tables query
-    // var query = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=', opts);
-
-    // // Set basic graph options
-    // var options = {
-    //     title: "Frequency of " + chartText.title + " Amongst All Crabs",
-    //     vAxis: {
-    //         title: "Frequency"
-    //     },
-    //     hAxis: {
-    //         title: chartText.hAxis
-    //     },
-    //     // Group buckets by 5
-    //     histogram: {
-    //         bucketSize: 5
-    //     }
-    // };
-
-    // // Get the specific data we want
-    // query.setQuery(chartText.queryStr);
-
-    // // Send the query with a callback function.
-    // query.send(handleQueryResponse);
-
     // The data comes from the aggroData array
     var data = google.visualization.arrayToDataTable(aggroData[chartText.tier]);
 
@@ -77,7 +32,6 @@ function drawVisualization() {
         },
         legend: {
             position: 'right',
-            maxLines: 3
         },
         bar: {
             groupWidth: '75%'
